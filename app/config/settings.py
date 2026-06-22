99a6e13f90c01e34410cc2b1a665af2be3312b48
Victor Reipur <vicre@dtu.dk>
2026-06-22 14:31:50 +0200

    block overwriting if else logic

--- app/config/settings.py
+++ app/config/settings.py
@@ -124,15 +124,6 @@ else:
     }
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
